e3a6c8862ab1f6b312fa1b6c4eb030b7cc9c6e32
SergiuCon1 <sergiucontevici@gmail.com>
2022-06-05 20:32:19 +0300

    JUnit5 and Legacy

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,12 @@
 package ru.netology.service;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -52,6 +56,7 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
+    @Ignore
     @Test
     public void shouldGetRemains0WhenAmount1000() {
 
@@ -83,5 +88,85 @@ public class CashbackHackServiceTest {
         int expected = 1000;
 
         assertEquals(actual, expected);
+    }
+
+    //API JUnit Jupiter
+
+    @org.junit.jupiter.api.Test
+    public void shouldGetRemains200API() {
+
+        int amount = 800;
+
+        int actual = service.remain(amount);
+        int expected = 200;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldGetRemains1000API() {
+
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldGetRemains1100API() {
+
+        int amount = -100;
+
+        int actual = service.remain(amount);
+        int expected = 1100;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldGetRemains1API() {
+
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Disabled
+    @org.junit.jupiter.api.Test
+    public void shouldGetRemains0WhenAmount1000API() {
+
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldGetRemains999API() {
+
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldGetRemains1000WhenBigAmountAPI() {
+
+        int amount = 1000000000;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        Assertions.assertEquals(actual, expected);
     }
 }
